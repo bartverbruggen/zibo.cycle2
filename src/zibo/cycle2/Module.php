c7f2d4a07e2b41b19abf15c2b8a52869ec0553f7
Bart Verbruggen <bart@statik.be>
2013-02-19 20:29:21 +0100

    Init commit

---
+++ src/zibo/cycle2/Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace zibo\cycle2;
+
+/**
+ * FlexSlider module: container of the scripts and style constants
+ */
+class Module {
+
+    /**
+     * Path to the JS of FlexSlider
+     * @var string
+     */
+    const SCRIPT_CYCLE2 = 'js/cycle2/jquery.cycle2.js';
+
+}
